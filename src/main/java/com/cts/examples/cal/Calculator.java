ff182f57d28813c00ca930da62f156f0accc3b46
montapas <montapas@gmail.com>
2018-08-29 17:39:05 +0530

    Update Calculator.java

--- src/main/java/com/cts/examples/cal/Calculator.java
+++ src/main/java/com/cts/examples/cal/Calculator.java
@@ -4,10 +4,7 @@ public class Calculator implements ICalculator {
 
 	 
 	public int sum(int a, int b) {
-		   int j;
-   		 while (true) {  
-   		   j++;
- 		 }
+		 
 		return a + b;
 		
 		
@@ -15,7 +12,10 @@ public class Calculator implements ICalculator {
 
 	 
 	public int subtraction(int a, int b) {
-		
+		  int j;
+   		 while (true) {  
+   		   j++;
+ 		 }
 		return a - b;
 	}
 
