e302bf79b29d64e0b39221e5f5c0e01f56a5be9c
montapas <montapas@gmail.com>
2018-10-08 15:22:30 +0530

    Update Calculator.java

--- src/main/java/com/cts/examples/cal/Calculator.java
+++ src/main/java/com/cts/examples/cal/Calculator.java
@@ -3,13 +3,7 @@ package com.cts.examples.cal;
 public class Calculator implements ICalculator {
 
 	public int sum(int a, int b) {
-		int j=0;
-		int x=0;
-		if (x == 0) {
-		 j=1;
-		} else if (x == 1) {
-		 j=2;
-		}
+		
 		return a + b;
 	}
 	 
