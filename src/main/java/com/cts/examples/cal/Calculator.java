9b2e1e64b8ebf6b58e78d9e6efdcc0b8b89f2292
montapas <montapas@gmail.com>
2018-10-09 10:54:24 +0530

    s

--- src/main/java/com/cts/examples/cal/Calculator.java
+++ src/main/java/com/cts/examples/cal/Calculator.java
@@ -4,7 +4,6 @@ public class Calculator implements ICalculator {
 
 	public int sum(int a, int b) {
 
-		System.out.println(String.format("The value of my integer is %d", "Hello World"));  // Noncompliant; an 'int' is expected rather than a String)
 		 
 		return a + b;  
 	}
