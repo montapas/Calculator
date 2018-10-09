47eabe78114c7b5491033eb73e82322b3d7c1670
montapas <montapas@gmail.com>
2018-10-09 10:30:54 +0530

    ssds

--- src/main/java/com/cts/examples/cal/Calculator.java
+++ src/main/java/com/cts/examples/cal/Calculator.java
@@ -4,7 +4,10 @@ public class Calculator implements ICalculator {
 
 	public int sum(int a, int b) {
 
-		
+
+		int target = -5;
+		int num = 3;
+		target =+ num; // Noncompliant; target = 3
 		 return a + b;  
 	}
 	 
