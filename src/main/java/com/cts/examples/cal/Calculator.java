c59edc2f963c2897a08280762dabba46f99e6916
montapas <montapas@gmail.com>
2018-12-06 15:11:53 +0530

    Update Calculator.java

--- src/main/java/com/cts/examples/cal/Calculator.java
+++ src/main/java/com/cts/examples/cal/Calculator.java
@@ -3,28 +3,22 @@ package com.cts.examples.cal;
 public class Calculator implements ICalculator {
 
 	public int sum(int a, int b) {
-		 System.out.println(String.format("The value of my integer is %d", "Hello World"));  // Noncompliant; an 'int' is expected rather than a String)
-		while(true){
-            		print("printing...");
-       		 }
+		
 		return a + b;  
 	}
 	 
 	public int subtraction(int a, int b) {
-		while(true){
-            		print("printing...");
-       		 }
+		
 	       return a - b;
 	}
 	 
 	public int multiplication(int a, int b) {
-		 while(true){
-            		print("printing...");
-       		 }
+		 
 		return a * b;
 	}
 
 	public int divison(int a, int b)  {
+		 System.out.println(String.format("The value of my integer is %d", "Hello World"));  // Noncompliant; an 'int' is expected rather than a String)
 		
 		return a / b;
 	}
